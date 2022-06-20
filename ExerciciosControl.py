90c200e395e3e3e15bf20e19b1b40ce075443803
sobralAllan <allan.sobraldasilva@gmail.com>
2022-06-20 19:58:49 -0300

    Atualização

--- ExerciciosControl.py
+++ ExerciciosControl.py
@@ -1,6 +1,8 @@
 import this
 import ExerciciosModel
 this.opcao = -1
+this.bas = -1
+this.altura = -1
 
 def Menu():
     print('Menu\n\n'          +
@@ -19,6 +21,22 @@ def Executar():
         elif this.opcao == 1:
             print(ExerciciosModel.exercicio01())
         elif this.opcao == 2:
-            print('Escreva aqui a solução para o exercício 02')
+            print('Informe um número')
+            num1 = int(input())
+            print(ExerciciosModel.exercicio02(num1))
+        elif this.opcao == 3:
+            while this.bas <= 0:
+                print('Informe a base do retângulo: ')
+                this.bas = float(input())
+                if this.bas <= 0:
+                    print('Informe uma base com valor positivo')
+
+            while this.altura <= 0:
+                print('Informe a altura do retângulo: ')
+                this.altura = float(input())
+                if this.altura <= 0:
+                    print('Informe uma base com valor positivo')
+
+            print(ExerciciosModel.exercicio03(this.bas,this.altura))
         else:
             print('Opção informada não é válida!')
--- ExerciciosModel.py
+++ ExerciciosModel.py
@@ -1,3 +1,4 @@
+
 def exercicio01():
     A = 10
     B = 20
@@ -8,3 +9,8 @@ def exercicio01():
     msg = msg + "\nDepois da Troca: A = {}, B = {}".format(A,B)
     return msg
 
+def exercicio02(num1):
+    return 'O antecessor é {}'.format(num1-1)
+
+def exercicio03(bas, altura):
+    return bas * altura
